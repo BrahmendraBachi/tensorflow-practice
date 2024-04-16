6b2e46132867cacc5722e562d6c18281a80badfa
hari <hari8185008552@gmail.com>
2024-04-17 00:21:10 +0530

    change commonUtils

--- IDLE/commons/commonUtils.py
+++ IDLE/commons/commonUtils.py
@@ -5,9 +5,11 @@ import numpy as np
 
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
-def plotForOneResult(x_labelData, y_labelData, x_label='', y_label='', labelName='', title='', symbolToPlot='b', isShow=False):
+
+def plotForOneResult(x_labelData, y_labelData, x_label='', y_label='', labelName='', title='', symbolToPlot='b',
+                     isShow=False):
     plt.plot(x_labelData, y_labelData, symbolToPlot, label=labelName)
-    plt.xlabel(x_label) 
+    plt.xlabel(x_label)
 
     plt.title(title)
     plt.ylabel(y_label)
@@ -16,7 +18,9 @@ def plotForOneResult(x_labelData, y_labelData, x_label='', y_label='', labelName
     if isShow:
         plt.show()
 
-def plotForTwoResults(x_data, y1_data, y2_data, x_label='', y_label='', y1_label='', y2_label='', title='', isLegend=True, isShow=False, isGrid = True):
+
+def plotForTwoResults(x_data, y1_data, y2_data, x_label='', y_label='', y1_label='', y2_label='', title='',
+                      isLegend=True, isShow=False, isGrid=True):
     plt.plot(x_data, y1_data, 'bo', label=y1_label)
     plt.plot(x_data, y2_data, 'b', label=y2_label)
     plt.title(title)
@@ -28,14 +32,13 @@ def plotForTwoResults(x_data, y1_data, y2_data, x_label='', y_label='', y1_label
     if isShow:
         plt.show()
 
-def plot_for_one_model(history, isValidation=False):
 
+def plot_for_one_model(history, isValidation=False):
     epochs = range(1, len(history.history['loss']) + 1)
 
     training_loss = history.history['loss']
     training_accuracy = history.history["acc"]
     plt.figure(figsize=(12, 4))
-
 
     if isValidation:
         validation_loss = history.history["val_loss"]
@@ -95,8 +98,8 @@ def plot_for_one_model(history, isValidation=False):
 
     plt.show()
 
-def plot_for_two_model(history1, history2, isValidation=False, with_combined=False):
 
+def plot_for_two_model(history1, history2, isValidation=False, with_combined=False):
     training_loss1 = history1.history['loss']
     training_accuracy1 = history1.history['acc']
 
@@ -169,10 +172,9 @@ def plot_for_two_model(history1, history2, isValidation=False, with_combined=Fal
     else:
 
         epochs_1 = range(1, len(history1.history['val_loss']) + 1)
-        epochs_2 =range(1, len(history2.history['val_loss']) + 1)
+        epochs_2 = range(1, len(history2.history['val_loss']) + 1)
 
         if isValidation:
-
             validation_loss1 = history1.history['val_loss']
             validation_accuracy1 = history1.history['val_acc']
 
@@ -191,7 +193,6 @@ def plot_for_two_model(history1, history2, isValidation=False, with_combined=Fal
                 y2_label="Validation Loss_1",
                 title="Training and Validation Losses for Model_1"
             )
-
 
             plt.subplot(2, 2, 2)
 
@@ -233,6 +234,7 @@ def plot_for_two_model(history1, history2, isValidation=False, with_combined=Fal
             )
     plt.show()
 
+
 def plot_image(image, isShow=True):
     plt.gray()
     plt.axis('off')
@@ -241,9 +243,10 @@ def plot_image(image, isShow=True):
     if isShow:
         plt.show()
 
+
 def visualize_model(model, img):
     successive_outputs = [layer.output for layer in model.layers[1:]]
-    visualization_model = tf.keras.models.Model(inputs = model.input, outputs = successive_outputs)
+    visualization_model = tf.keras.models.Model(inputs=model.input, outputs=successive_outputs)
 
     successive_feature_maps = visualization_model.predict(img)
     layer_names = [layer.name for layer in model.layers[1:]]
@@ -257,7 +260,6 @@ def visualize_model(model, img):
             display_grid = np.zeros((size, size * n_features))
 
             for i in range(n_features):
-
                 x = feature_map[0, :, :, i]
                 x -= x.mean()
                 x /= x.std()
@@ -266,13 +268,14 @@ def visualize_model(model, img):
 
                 x = np.clip(x, 0, 255).astype('uint8')
 
-                display_grid[:, i * size : (i + 1) * size] = x
+                display_grid[:, i * size: (i + 1) * size] = x
 
-            scale = 20./n_features
+            scale = 20. / n_features
             plt.figure(figsize=(scale * n_features, scale))
             plt.title(layer_name)
             plt.grid(False)
             plt.imshow(display_grid, aspect='auto', cmap='viridis')
+
 
 def plot_for_multiple_models(histories):
     histories = [history.history for history in histories]
@@ -291,7 +294,7 @@ def plot_for_multiple_models(histories):
         axs[1, 0].plot(epochs, validation_loss, label=label_name)
         axs[1, 1].plot(epochs, validation_acc, label=label_name)
 
-    titleNames = [["Training Losses", "Traning Accuracies",], ["Validation Losses", "Validation Accuracies"]]
+    titleNames = [["Training Losses", "Traning Accuracies", ], ["Validation Losses", "Validation Accuracies"]]
     for i in range(2):
         for j in range(2):
             axs[i, j].grid(True)
@@ -300,7 +303,7 @@ def plot_for_multiple_models(histories):
     plt.show()
 
 
-class myCallback(tf.keras.callbacks.Callback):
+class MyCallback(tf.keras.callbacks.Callback):
 
     def __init__(self):
         self.all_model_weights = []
@@ -311,7 +314,7 @@ class myCallback(tf.keras.callbacks.Callback):
         self.epochs = None
 
     def on_epoch_end(self, epochs, logs={}):
-        print()
+        print("\n")
         self.all_model_weights.append(self.model.get_weights())
         self.all_logs.append(logs)
         if logs.get("val_acc") > self.max_val_acc:
@@ -321,8 +324,8 @@ class myCallback(tf.keras.callbacks.Callback):
             self.epochs = epochs
             print(f"max_val_acc changed at epoch: {epochs}")
 
-        print(f"Val_Accuracy at epoch: {logs.get('val_acc')}")
-        print(f"Maximum Val_Accuracy at epoch: {self.max_val_acc}")
+        print(f"Val_Accuracy at epoch {epochs}: {logs.get('val_acc')}")
+        print(f"Maximum Val_Accuracy at epoch {epochs}: {self.max_val_acc}")
 
     def get_efficient_model_weights(self):
         return self.eff_model_weights
@@ -334,7 +337,4 @@ class myCallback(tf.keras.callbacks.Callback):
         return self.all_logs
 
     def get_all_model_weights(self):
-        return self.all_models_weights
-
-
-callbacks = myCallback()
+        return self.all_model_weights
